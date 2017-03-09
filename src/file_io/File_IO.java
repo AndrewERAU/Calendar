24371c0744c5263b3e09705c6b3cf9ccbb14f5a2
Pinto18 <pinto18.nd@gmail.com>
2017-03-08 20:28:10 -0700

    Created test for exporting ics file to text file

--- src/file_io/File_IO.java
+++ src/file_io/File_IO.java
@@ -1,5 +1,31 @@
 package file_io;
 
-public class File_IO {
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 
+import event.Event;
+
+public class File_IO
+{
+    /*public void IcsImportTextFile(File importFile)  //pass a text file in as an argument to be converted into an ics file
+     {
+     //create a scanner which will read each line in the file
+     //scanner will search for words: "Title", "Info", "Start time", "End time"
+     //when those key words are detected, the scanner will read the rest of the line of the file to obtain the event information and store it in a matching variable
+     //next the program will use the newly created variables to create a new event object
+     //after the event is created, then the program will export the event as an ics file using the IcsExport function defined above
+     //if the program contains any invalid input or missing event fields, then raise exception which prompts user to enter the missing information
+     }*/
+    
+    //method for exporting ics files to text files
+    /*public void IcsExportToTextFile(File IcsFile)
+    {
+        //open ICS file
+        //open new text file to output event information to
+        //create scanner to read ics file
+        //scan contents of ics for strings that match "Title", "Info", "Start Date", "End Date", "Start Time", "End Time"
+        //once a keyword is found, save the string and output it to the text file with a matching detail tag
+        //Incremental Feature: Print entire calendar objects containing events to a text file
+    }*/
 }
