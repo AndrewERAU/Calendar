0f3771f3e110d7d31ea9cb03fdd46dd743c6c1e7
Pinto18 <pinto18.nd@gmail.com>
2017-04-05 10:40:04 -0700

    Nick is Updating

--- src/event/Reminder.java
+++ src/event/Reminder.java
@@ -1,30 +1,30 @@
-package event;
+package reminder;
 
-import java.util.Scanner;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class Reminder
 {
-	// This class will handle sending events to server
-	private String eventTitle = null;
-	private String eventInfo = null;
-	private Integer firstAlertTime = null;
+    // This class will handle sending events to server
+    private String eventTitle = null;
+    private String eventInfo = null;
+    private Integer firstAlertTime = null;
     private Integer secondAlertTime = null;
-	
-	public Reminder()
+    
+    public Reminder()
     {
-		eventTitle = "";
-		eventInfo = "";
-		firstAlertTime = -1;
+        eventTitle = "";
+        eventInfo = "";
+        firstAlertTime = -1;
         secondAlertTime = -1;
         System.out.println("Reminder object was created.");
-	}
-	
-	public String getTitle()
+    }
+    
+    
+    public String getTitle()
     {
         return this.eventTitle;
-	}
+    }
     
     public String getInfo()
     {
@@ -40,11 +40,11 @@ public class Reminder
     {
         return this.secondAlertTime;
     }
-	
-	public void setTitle(String title)
+    
+    public void setTitle(String title)
     {
         this.eventTitle = title;
-	}
+    }
     
     public void setEventInfo(String info)
     {
@@ -54,22 +54,39 @@ public class Reminder
     public void setFirstAlert(Integer alertTime)
     {
         this.firstAlertTime = alertTime;
-        System.out.println("Setting first reminder:");
-        System.out.println("Converting the inputted alertTime into the amount of seconds until alertTime occurs");
-        System.out.println("Setting a Timer class object to countdown from when user indicated they want the reminder to appear");
-        System.out.println("When timer reaches zero, reminder will appear on terminal.");
-        //Integer delay = alertTime * 1000;
-        //Integer period = 1000;
-        //timer = new Timer();
-        //timer.scheduleAtFixedR
+        
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask()
+                       {
+            @Override
+            public void run()
+            {
+                System.out.print("\nEvent: Reminder Test 1 First Alert");
+                System.out.print("Current time is: ");
+                System.out.println(System.currentTimeMillis()/1000);
+            }
+        }, alertTime * 1000  //converts alert time from milliseconds to seconds
+                       );
+        //Next step: convert alertTime from inputted as hh:mm:ss to seconds until event occurs
     }
     
     public void setSecondAlert(Integer alertTime)
     {
-        this.secondAlertTime = alertTime;
-        System.out.println("Setting second reminder:");
-        System.out.println("Converting the inputted alertTime into the amount of seconds until alertTime occurs");
-        System.out.println("Setting a Timer class object to countdown from when user indicated they want the reminder to appear");
-        System.out.println("When timer reaches zero, reminder will appear on terminal.");
+        this.firstAlertTime = alertTime;
+        
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask()
+                       {
+            @Override
+            public void run()
+            {
+                System.out.print("\nEvent: Reminder Test 1 Second Alert");
+                System.out.print("Current time is: ");
+                System.out.println(System.currentTimeMillis()/1000);
+            }
+        }, alertTime * 1000  //converts alert time from milliseconds to seconds
+                       );
+        //Next step: convert alertTime from inputted as hh:mm:ss to seconds until event occurs
     }
 }
+
