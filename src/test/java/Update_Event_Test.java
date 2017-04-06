6a2e7c228aa8a683c2ef1f91aa356c9a7995c308
Andrew Sverdrup <sverdra1@my.erau.edu>
2017-04-06 15:48:55 -0700

    added JUnit test for updating events

---
+++ src/test/java/Update_Event_Test.java
@@ -0,0 +1,41 @@
+package test.java;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+
+import org.junit.Test;
+
+import database.DatabaseMgr;
+import event.Event;
+
+public class Update_Event_Test {
+	private Event event = new Event("My favorite event", // eventTitle
+			"Meeting to go over plan details.", // eventDescription
+			"2017-05-20", // eventDate
+			"12:30:00", // eventStartTime
+			"13:30:00", // eventEndTime
+			"3500 Deer Creek Rd, Palo Alto, CA 94304", // eventLocation
+			"", // eventInvitees
+			"Work", // eventTag
+			"",
+			"");
+	
+	@Test
+	public void test() {
+		File dbFile = new File(DatabaseMgr.TEST_DB_PATH);
+		dbFile.delete();
+		DatabaseMgr db = new DatabaseMgr();
+		
+		assertEquals(db.retrieveEvents('A',"").size(),0); // should be 0 to start
+		event = db.insertEvent(event); // should succeed
+		assertEquals(db.retrieveEvents('T',"My favorite event").size(),1); // now 1 element
+		
+		event.setEventTitle("Meeting");
+		
+		event = db.updateEvent(event); // should succeed
+		assertEquals(db.retrieveEvents('T',"My favorite event").size(),0); // now 0 element
+		assertEquals(db.retrieveEvents('T',"Meeting").size(),1); // now 1 element
+		assertEquals(db.retrieveEvents('A',"").size(),1); // should be 0 to start
+	}
+}
