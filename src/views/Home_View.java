04f64419c1c078eaf7e187e3fc36ad13732b4713
Andrew Sverdrup <sverdra1@my.erau.edu>
2017-04-08 16:43:28 -0700

    Created main calendar GUI

--- src/views/Home_View.java
+++ src/views/Home_View.java
@@ -1,11 +1,16 @@
 package views;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.UIManager; // to set look and feel
+import javax.swing.border.LineBorder;
 
 import views.Add_Event_View;
 import views.Event_View;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 
 
@@ -15,17 +20,36 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
 
 
 public class Home_View {
 
 	private JFrame frame;
 	private JPanel panel;
+	private JPanel topPanel;
+	private JPanel optionsPanel;
+	private JPanel calPanel;
 	private JButton addEventButton;
 	//private Display_Add_Event_Screen addEventWindow;
 	
+    final int LABEL_WIDTH = 25;
+    final int LABEL_HEIGHT = 10;
+	
 	
 	public Home_View() {
+		try {
+		    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		      //UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+		} catch (Exception e) {
+		// exit application, log  or ignore exception
+			System.out.println("setting look and feel failed.");
+		}
 
         initUI();
     }
@@ -38,21 +62,51 @@ public class Home_View {
 
     private void initUI() {
     	
-    	Calendar_View cal = new Calendar_View();
+    	//Calendar_View cal = new Calendar_View();
+    	// TODO: Remove calendar code in other file
     	
     	
     	frame = new JFrame();
         frame.setTitle("Planner");
-        frame.setSize(900, 600);     
+        frame.setSize(720, 590); // width, height    
         frame.setResizable(false);
         frame.setLocationRelativeTo(null); // Show in middle of screen
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Exits app instead of just closing window
+        frame.getContentPane().setLayout(null);
         
-        panel = new JPanel();
         
-        frame.add(panel);
+        //panel = new JPanel();
+        //panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        //panel.setLayout(null);
+        //optionsPanel = new JPanel();
+        //optionsPanel.setSize(frame.getWidth(), frame.getHeight()/18);
         
-        frame.getContentPane ().add (panel, BorderLayout.WEST);
+        calPanel = new JPanel();
+        calPanel.setLayout(new GridLayout(6,7,2,2)); // 6 rows, 7 cols, hgap = ?, vgap = ?
+        calPanel.setLocation(10, 55);
+        calPanel.setSize(700, 500);
+        
+        topPanel = new JPanel();
+        //topPanel.setLayout(new FlowLayout());
+        //topPanel.setLocation(10,10);
+        //topPanel.setSize(700, 120);
+        //topPanel.setBackground(Color.RED);
+        topPanel.setBounds(85,10, calPanel.getWidth() - 85 - 85, 35); // location(x,y) size(width,height)
+        topPanel.setLayout( new BorderLayout() );
+        
+        
+        calPanel.setBackground(Color.decode("#167ac6"));
+        
+        frame.add(calPanel);
+        frame.add(topPanel);
+        //frame.add(optionsPanel);
+        
+        //panel.add(optionsPanel);
+        //panel.add(calPanel);
+      
+        //frame.add(panel);
+        
+        //frame.getContentPane ().add (panel, BorderLayout.WEST);
         
         addEventButton = new JButton("Add Event");
         addEventButton.addActionListener(new ActionListener()
@@ -65,11 +119,139 @@ public class Home_View {
         });
         
         //frame.getContentPane().setLayout(new FlowLayout());
-        panel.add(addEventButton);
+        //panel.add(addEventButton); // TODO: uncomment this once calendar is done
+        
+        // Start calendar code -------------------------------------------------------------------------------------------------------------------
+
+        createNextPrvMonthButton("<",BorderLayout.WEST);
+        addMonthYearLabel();
+        createNextPrvMonthButton(">",BorderLayout.EAST);
+        
+        
+        createDayOfWeekLabel("Sunday",Color.decode("#167ac6"));
+        createDayOfWeekLabel("Monday",Color.decode("#167ac6"));
+        createDayOfWeekLabel("Tuesday",Color.decode("#167ac6"));
+        createDayOfWeekLabel("Wednesday",Color.decode("#167ac6"));
+        createDayOfWeekLabel("Thursday",Color.decode("#167ac6"));
+        createDayOfWeekLabel("Friday",Color.decode("#167ac6"));
+        createDayOfWeekLabel("Saturday",Color.decode("#167ac6"));
+        
+        
+        addWhiteBoxesUnderWeekLabels();
+        
         
         
         frame.setVisible(true);
     }
+    
+    private void createDayOfWeekLabel(String labelText, Color bg) {
+
+        JLabel inLabel = new JLabel(labelText, SwingConstants.CENTER);
+        
+        inLabel.setFont(new Font("", Font.PLAIN, 16));
+        
+      //sundayLabel.setSize(new Dimension(50,50));
+        inLabel.setMinimumSize(new Dimension(LABEL_WIDTH, LABEL_HEIGHT));
+        inLabel.setPreferredSize(new Dimension(LABEL_WIDTH, LABEL_HEIGHT));
+        inLabel.setMaximumSize(new Dimension(LABEL_WIDTH, LABEL_HEIGHT));
+        inLabel.setForeground(Color.white); // text color
+        inLabel.setBackground(bg);
+        inLabel.setOpaque(true); // without this the background color is not shown
+        
+        calPanel.add(inLabel);
+    }
+    
+    private void addWhiteBoxesUnderWeekLabels() {
+    	// INFO: The "white boxes" are buttons that represent days of the month
+    	
+    	for (int i = 0; i < 35; i++) {
+        	JButton button = new JButton(Integer.toString(i));
+        	button.setMinimumSize(new Dimension(LABEL_WIDTH, LABEL_HEIGHT));
+            button.setPreferredSize(new Dimension(LABEL_WIDTH, LABEL_HEIGHT));
+            button.setMaximumSize(new Dimension(LABEL_WIDTH, LABEL_HEIGHT));
+            button.setBorder( new LineBorder(Color.white) );
+            button.setBackground(Color.WHITE);
+            button.setOpaque(true); // without this the background color is not shown
+            
+            // Perform action when button is pressed
+            button.addActionListener(new ActionListener()
+            {
+              public void actionPerformed(ActionEvent e)
+              {
+                // display new add event window
+            	  new Add_Event_View();
+              }
+            });
+            
+            // Got this from SO, but it looks like there is a better way (see comments on accepted answer)
+            // http://stackoverflow.com/questions/22638926/how-to-put-hover-effect-on-jbutton
+            button.addMouseListener(new java.awt.event.MouseAdapter() {
+                public void mouseEntered(java.awt.event.MouseEvent evt) {
+                    button.setBorder( new LineBorder(Color.BLUE,2,false)); // color, thickness, rounded corners
+                	button.setCursor(new Cursor(Cursor.HAND_CURSOR));
+                }
+
+                public void mouseExited(java.awt.event.MouseEvent evt) {
+                	button.setBorder( new LineBorder(Color.white));
+                }
+            });
+        	
+        	calPanel.add(button);
+    	}
+
+    }
+    
+    private void createNextPrvMonthButton(String buttonText, String position) {
+    	final int width = 70;
+    	final int height = 30;
+
+        JButton button = new JButton(buttonText);
+        
+        button.setFont(new Font("", Font.PLAIN, 16));
+        
+        //button.setSize(new Dimension(LABEL_WIDTH, LABEL_HEIGHT));
+        button.setMinimumSize(new Dimension(width, height));
+        button.setPreferredSize(new Dimension(width, height));
+        button.setMaximumSize(new Dimension(width, height));
+        button.setForeground(Color.BLACK); // text color
+        button.setBackground(Color.WHITE);
+        button.setOpaque(true); // without this the background color is not shown
+        button.setBorder( new LineBorder(Color.BLACK,2,false)); // color, thickness, rounded corners
+        
+        // Got this from SO, but it looks like there is a better way (see comments on accepted answer)
+        // http://stackoverflow.com/questions/22638926/how-to-put-hover-effect-on-jbutton
+        button.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+            	button.setCursor(new Cursor(Cursor.HAND_CURSOR));
+            }
+/*
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+            	button.setBorder( new LineBorder(Color.BLACK,2,false)); // color, thickness, rounded corners
+            }*/
+        });
+        
+        topPanel.add(button, position);
+    }
+    
+    private void addMonthYearLabel() {
+    	final int width = 30;
+    	final int height = 50;
+    	JLabel label = new JLabel("April, 2017",SwingConstants.CENTER);
+    	
+    	label.setFont(new Font("", Font.PLAIN, 24));
+         
+        //sundayLabel.setSize(new Dimension(50,50));
+        label.setMinimumSize(new Dimension(width, height));
+        label.setPreferredSize(new Dimension(width, height));
+        label.setMaximumSize(new Dimension(width, height));
+	    //label.setForeground(Color.white); // text color
+	    //label.setBackground(bg);
+	    // label.setOpaque(true); // without this the background color is not shown
+        
+		
+    	topPanel.add(label,BorderLayout.CENTER);
+    }
+
     
 
 }
