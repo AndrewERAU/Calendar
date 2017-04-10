bb6ec77123aa78dcb858c39e6972a6163da69b49
lefflerk <lefflerk@my.erau.edu>
2017-04-10 15:56:59 -0700

    Fixed bug for Issue 31

--- src/time/Time.java
+++ src/time/Time.java
@@ -10,12 +10,41 @@ public class Time {
 	
 	public static String getMonthYear(){
 		int year; 
-		int month; 
+		int month;
+		String monthString = "";
 		Date date = new Date();
 		LocalDate localDate = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
 		year = localDate.getYear();
 		month =localDate.getMonthValue();
-		String output = Integer.toString(month) + ", " + Integer.toString(year);
+		
+		switch (month){
+		case 1:
+			monthString = "January";
+		case 2:
+			monthString = "February";
+		case 3:
+			monthString = "March";
+		case 4:
+			monthString = "April";
+		case 5:
+			monthString = "May";
+		case 6:
+			monthString = "June";
+		case 7:
+			monthString = "July";
+		case 8:
+			monthString = "August";
+		case 9:
+			monthString = "September";
+		case 10:
+			monthString = "October";
+		case 11:
+			monthString = "November";
+		case 12:
+			monthString = "Decmeber";
+		}
+		
+		String output = monthString + ", " + Integer.toString(year);
 		return output;
 	}
 	
