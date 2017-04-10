fc28eebbd9f17e30282ac0bb32ef27ecf3be3981
lefflerk <lefflerk@my.erau.edu>
2017-04-10 15:46:55 -0700

    Added Month, Year return function

--- src/time/Time.java
+++ src/time/Time.java
@@ -1,5 +1,22 @@
 package time;
 
-public class Time {
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
 
+public class Time {
+	int year; 
+	int month; 
+	
+	
+	public String getMonthYear(){
+		Date date = new Date();
+		LocalDate localDate = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+		year = localDate.getYear();
+		month =localDate.getMonthValue();
+		String output = Integer.toString(month) + ", " + Integer.toString(year);
+		return output;
+	}
+	
+	
 }
