37e3c5821ec4a125fc55f89f883e8ce2732337a0
Andrew Sverdrup <sverdra1@my.erau.edu>
2017-04-11 12:23:06 -0700

    Added support for datetime for reminder integration

--- src/event/Event.java
+++ src/event/Event.java
@@ -1,5 +1,10 @@
 package event;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class Event {
 	private String eventID;
 	private String eventTitle;
@@ -10,8 +15,10 @@ public class Event {
 	private String eventLocation;
 	private String eventInvitees;
 	private String eventTag;
-	private String eventReminder1;
-	private String eventReminder2;
+	private String eventReminder1Date;
+	private String eventReminder1Time;
+	private String eventReminder2Date;
+	private String eventReminder2Time;
 	
 	public Event() {
 		this.eventID = null;
@@ -23,8 +30,10 @@ public class Event {
 		this.eventLocation = null;
 		this.eventInvitees = null;
 		this.eventTag = null;
-		this.eventReminder1 = null;
-		this.eventReminder2 = null;
+		this.eventReminder1Date = null;
+		this.eventReminder1Time = null;
+		this.eventReminder2Date = null;
+		this.eventReminder2Time = null;
 	}
 	
 	public Event(String eventTitle,
@@ -35,8 +44,10 @@ public class Event {
 			  String eventLocation,
 			  String eventInvitees,
 			  String eventTag,
-			  String eventReminder1,
-			  String eventReminder2) {
+			  String eventReminder1Date,
+			  String eventReminder1Time,
+			  String eventReminder2Date,
+			  String eventReminder2Time) {
 		
 		this.eventID = null;
 		if (!"".equals(eventTitle)) {
@@ -78,19 +89,29 @@ public class Event {
 			this.eventTag = eventTag;
 		} else {
 			this.eventTag = "NULL";
-		}
-		if (!"".equals(eventReminder1)) {
-			this.eventReminder1 = eventReminder1;
+		}	
+		if (!"".equals(eventReminder1Date)) {
+			this.eventReminder1Date = eventReminder1Date;
 		} else {
-			this.eventReminder1 = "NULL";
+			this.eventReminder1Date = "NULL";
 		}
-		if (!"".equals(eventReminder2)) {
-			this.eventReminder2 = eventReminder2;
+		if (!"".equals(eventReminder1Time)) {
+			this.eventReminder1Time = eventReminder1Time;
 		} else {
-			this.eventReminder2 = "NULL";
+			this.eventReminder1Time = "NULL";
+		}
+		if (!"".equals(eventReminder2Date)) {
+			this.eventReminder2Date = eventReminder2Date;
+		} else {
+			this.eventReminder2Date = "NULL";
+		}
+		if (!"".equals(eventReminder2Time)) {
+			this.eventReminder2Time = eventReminder2Time;
+		} else {
+			this.eventReminder2Time = "NULL";
 		}
 	}
-	
+	/* TODO: remove this
 	public Event addSingleQuotes() {
 		Event event = new Event();
 		if (!"NULL".equals(this.eventTitle)) {
@@ -128,7 +149,7 @@ public class Event {
 
 		return event;
 	}
-	
+	*/
 	
 	// Getters
 	public String getEventID() {
@@ -158,11 +179,37 @@ public class Event {
 	public String getEventTag() {
 		return eventTag;
 	}
-	public String getEventReminder1() {
-		return eventReminder1;
+	public String getEventReminder1Date() {
+		return eventReminder1Date;
 	}
-	public String getEventReminder2() {
-		return eventReminder2;
+	public String getEventReminder1Time() {
+		return eventReminder1Time;
+	}
+	public String getEventReminder2Date() {
+		return eventReminder2Date;
+	}
+	public String getEventReminder2Time() {
+		return eventReminder2Time;
+	}
+	public Date getEventStartDateTime() {
+		Date date = null;
+		try {
+			date = new SimpleDateFormat("HH:mm yyyy-M-d").parse(this.getEventStartTime() + " " + this.getEventDate());
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+		return date;
+	}
+	public Date getEventEndDateTime() {
+		Date date = null;
+		try {
+			date = new SimpleDateFormat("HH:mm yyyy-M-d").parse(this.getEventEndTime() + " " + this.getEventDate());
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+		return date;
 	}
 	
 	// Setters
@@ -193,10 +240,16 @@ public class Event {
 	public void setEventTag(String tag) {
 		eventTag =  tag;
 	}
-	public void setEventReminder1(String reminder) {
-		eventReminder1 = reminder;
+	public void setEventReminder1Date(String reminder) {
+		eventReminder1Date = reminder;
 	}
-	public void setEventReminder2(String reminder) {
-		eventReminder2 = reminder;
+	public void setEventReminder1Time(String reminder) {
+		eventReminder1Time = reminder;
+	}
+	public void setEventReminder2Date(String reminder) {
+		eventReminder2Date = reminder;
+	}
+	public void setEventReminder2Time(String reminder) {
+		eventReminder2Time = reminder;
 	}
 }
