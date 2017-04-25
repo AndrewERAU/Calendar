634819dff26282e4c4cb0e6c165f608b7b58098f
Pinto18 <pinto18.nd@gmail.com>
2017-04-25 12:46:45 -0700

    Improving Code Coverage for Reminder Test

--- src/reminder/Reminder.java
+++ src/reminder/Reminder.java
@@ -25,12 +25,16 @@ public class Reminder
         return this.alertTime;
     }
     
-    public void setAlert(Event event, Integer numDays, Integer numHours, Integer numMinutes) throws FileNotFoundException
+    public void setAlert(Event event, Integer numDays, Integer numHours, Integer numMinutes) throws Exception
     {
 		final long MINUTE = 60000; // in milli-seconds.
 		final long HOUR = 60 * MINUTE; // in milli-seconds.
 		final long DAY = 24 * HOUR; // in milli-seconds
-		//SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm MM/dd/yyyy");
+		try
+		{
+		if(numDays < 0 | numHours < 0 | numMinutes < 0)
+			throw new Exception();  //exception handler for invalid reminder times
+		
 		SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm yyyy-M-d");
 		String event_content = "Event: " + event.getEventTitle() +
 							 "\nInfo: " + event.getEventDescription() +
@@ -39,20 +43,18 @@ public class Reminder
 							 "\nEnd Time: " + dateFormat.format(event.getEventEndDateTime());  //write out event info
 		String Filename = "reminder_message.txt";
 		PrintWriter pw = new PrintWriter(Filename);
-		// TODO: Add method to Event class to return date as Date type instead of String
-		// Then uncomment the next line
 		this.alertTime = new Date(event.getEventStartDateTime().getTime() - (numDays * DAY) - (numHours * HOUR) - (numMinutes * MINUTE));
 		pw.print("");
 		pw.print(event_content);
-		try {
           String command = "mail -s \"Event Reminder\" pinto18.nd@gmail.com < " + Filename + "| at " + dateFormat.format(alertTime);
 		  //Process p = Runtime.getRuntime().exec(command);
 			Process p = Runtime.getRuntime().exec("cat " + Filename + "| at " + dateFormat.format(alertTime));
             System.out.println(command);
-		} catch (IOException e) {
+        	pw.close();
+		} catch (Exception e) {
 			e.printStackTrace();
-		} 
-		pw.close();
+			System.out.println("Invalid Input");
+		}	
     }
 }
 
--- src/test/java/Reminder_Test.java
+++ src/test/java/Reminder_Test.java
@@ -10,9 +10,9 @@ import event.Event;
 public class Reminder_Test {
 
 	@Test
-	public void test() throws IOException {
-		 Reminder test_reminder_1 = new Reminder();
-		 Reminder test_reminder_2 = new Reminder();
+	public void test() throws Exception {
+		 Reminder test_reminder = new Reminder();
+		 Reminder negative_reminder = new Reminder();  //reminder created for fault injection
 		 Event event = new Event();
 		 SimpleDateFormat sdf = new SimpleDateFormat("HH:mm yyyy-MM-dd");
         
@@ -21,10 +21,10 @@ public class Reminder_Test {
 	     event.setEventDate("2017-04-24");
 	     event.setEventStartTime("12:00");
 	     event.setEventEndTime("13:00");
-	     test_reminder_1.setAlert(event, 0, 1, 0);  //set reminder to one hour before event
-	     test_reminder_2.setAlert(event, 0, 1, 5);  //set reminder to one hour and 5 minutes before event
-	     System.out.println("Reminder 1 is set to alert at: " + sdf.format(test_reminder_1.getAlert()));
-	     System.out.println("Reminder 2 is set to alert at: "+ sdf.format(test_reminder_2.getAlert()));
+	     test_reminder.setAlert(event, 0, 1, 0);  //set reminder to one hour before event
+	     negative_reminder.setAlert(event, -1, 1, 5);  //set reminder to invalid time to test exception handling
+	     System.out.println("Reminder 1 is set to alert at: " + sdf.format(test_reminder.getAlert()));
+	     //System.out.println("Reminder 2 is set to alert at: "+ sdf.format(negative_reminder.getAlert()));
 	     assertEquals(1,1);
 	}
 
