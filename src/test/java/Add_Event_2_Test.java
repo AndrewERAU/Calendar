306f7ec8f91c52faddbaea982459872fa06588f2
Andrew Sverdrup <sverdra1@my.erau.edu>
2017-04-28 14:41:45 -0700

    Add test to fix bug

---
+++ src/test/java/Add_Event_2_Test.java
@@ -0,0 +1,62 @@
+package test.java;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+
+import org.junit.Test;
+
+import database.DatabaseMgr;
+import event.Event;
+
+public class Add_Event_2_Test {
+
+
+	private Event event = new Event("Meeting", // eventTitle
+			"Meeting to go over plan details.", // eventDescription
+			"", // eventDate
+			"12:30", // eventStartTime
+			"13:30", // eventEndTime
+			"3500 Deer Creek Rd, Palo Alto, CA 94304", // eventLocation
+			"", // eventInvitees
+			"Work", // eventTag
+			"",
+			"",
+			"",
+			"");
+	
+	@Test
+	public void test() {
+		
+		
+		
+		File dbFile = new File(DatabaseMgr.TEST_DB_PATH);
+		dbFile.delete();
+		DatabaseMgr db = new DatabaseMgr();
+		
+		assertEquals(db.retrieveEvents('A',"").size(),0); // should be 0 to start
+		
+		event.setEventDate("2017-2-27");
+		db.insertEvent(event); // should succeed
+		assertEquals(db.retrieveEvents('D',"2017-02-27").size(),1); // now 1 element
+		assertEquals(db.retrieveEvents('A',"").size(),1); // should be 1 now
+
+		event.setEventDate("2017-2-01");
+		db.insertEvent(event); // should succeed
+		assertEquals(db.retrieveEvents('D',"2017-02-01").size(),1); // now 1 element
+		
+		event.setEventDate("2017-02-01");
+		db.insertEvent(event); // should succeed
+		assertEquals(db.retrieveEvents('D',"2017-02-27").size(),1); // now 1 element
+		
+		event.setEventDate("2017-02-1");
+		db.insertEvent(event); // should succeed
+		assertEquals(db.retrieveEvents('D',"2017-02-27").size(),1); // now 1 element
+		
+		event.setEventDate("2017-2-1");
+		db.insertEvent(event); // should succeed
+		assertEquals(db.retrieveEvents('D',"2017-02-27").size(),1); // now 1 element
+		
+	}
+
+}
