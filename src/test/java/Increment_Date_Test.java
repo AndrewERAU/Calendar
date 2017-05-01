cb10835bdea5460ff9f15e6ca6928c40982ddb1e
Andrew Sverdrup <sverdra1@my.erau.edu>
2017-04-30 18:23:36 -0700

    fix unit test

--- src/test/java/Increment_Date_Test.java
+++ src/test/java/Increment_Date_Test.java
@@ -11,7 +11,7 @@ public class Increment_Date_Test {
 	@Test
 	public void test() {
 		String testString = Time.incrementDate("2017-04-28");
-		assertEquals(testString.equals("2017-05-05"), true);//should be true
+		assertEquals(testString,"2017-05-05");//should be true
 	}
 
 }
